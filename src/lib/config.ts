f5f3ed6d64e6ebab00c2ec4a39d3af594a1c8be4
Arthur Bueno <arthurrnbueno@gmail.com>
2025-10-20 18:40:34 -0400

    fix: dados da empresa

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -2,7 +2,7 @@ export const config = {
   whatsapp: {
     number: '5567999532526', // Altere este número conforme necessário
     formatted: '+55 (67) 99953-2526', // Formato para exibição
-    url: 'https://wa.me/5567996101030' // URL completa do WhatsApp
+    url: 'https://wa.me/5567999532526' // URL completa do WhatsApp
   },
   company: {
     name: 'HaruCode',
